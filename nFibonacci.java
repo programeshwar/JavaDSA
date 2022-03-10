e9ed65b82195c57c12fe8ca53cdcf48f38877352
programeshwar <tiwary.rameshwaram@gmail.com>
2022-03-10 22:31:19 +0530

    N Fibonacci numbers

---
+++ nFibonacci.java
@@ -0,0 +1,21 @@
+import java.util.*;
+  
+  public class Main{
+  
+static void main(String[] args) {
+
+    Scanner scn = new Scanner(System.in);
+
+    int n = scn.nextInt();
+
+    int a=0,b=1;
+
+    for(int i=0; i<n; i++)
+    {
+      int c=a+b;
+      System.out.println(a);
+      a=b;
+      b=c;
+    }
+   }
+  }
